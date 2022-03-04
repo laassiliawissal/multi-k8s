fdba523b35cc269c165104b121aa7cae666a3df5
wissal.laassilia <wissal.laassilia@ibm.com>
2022-03-04 12:34:27 +0100

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            deploy react app on kubernetes
+            deploy react app up to kubernetes
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
